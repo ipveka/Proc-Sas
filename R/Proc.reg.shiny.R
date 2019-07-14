0c7d7fba874eeda75ca76ec6057d6b6a7b7f57a2
Ignasi <ignasi.pascual.vilaseca@gmail.com>
2019-07-14 19:56:21 +0200

    Add files via upload

--- R/Proc.reg.shiny.R
+++ R/Proc.reg.shiny.R
@@ -1,1 +1,15 @@
 # Proc.reg.shiny
+Proc.reg.shiny <- function(){
+
+   appDir <- system.file("Shiny", "myapp", package = "Proc.reg")
+   if (appDir == "") {
+      stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+   }
+
+   shiny::runApp(appDir, display.mode = "normal")
+}
+
+
+
+
+
